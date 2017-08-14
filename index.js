39c9b902fdd05ec07320b1280120248d00e9abde
harryfeng <2307555175@qq.com>
2017-08-13 22:45:29 -0400

    update

--- index.js
+++ index.js
@@ -10,9 +10,55 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+    response.render('pages/index');
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
+
+
+var request = require('request');
+
+var ArticleParser = require('article-parser');
+
+ArticleParser.configure({
+    timeout: 15 * 1000
+});
+(function go() {
+    request('http://9i9icenter.com/db/getUrl', function(error, response, body) {
+        if (error) {
+            setTimeout(function() {
+                go();
+            }, 15000);
+            console.log('error', error);
+        };
+        if (body && JSON.parse(body) && JSON.parse(body).url) {
+            ArticleParser.extract(JSON.parse(body).url).then(function(article) {
+                console.log('article.content.length', article.content.length);
+                if (article.content.length > 2000) {
+                    request.post({
+                        url: 'http://9i9icenter.com/db/addArticle',
+                        form: article
+                    }, function(error, response, body) {
+                        if (error) {
+                            console.log('error', error);
+                        }
+                        go();
+                    });
+                } else {
+                    go();
+                }
+            }).catch(function(error) {
+                console.log('ArticleParser catch error', error);
+                go();
+            }).finally(function() {
+
+            });
+        } else {
+            setTimeout(function() {
+                go();
+            }, 15000);
+        }
+    })
+})();

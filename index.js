ce0e2262fbfc304ac12d6665c5c743b23005179f
harryfeng <2307555175@qq.com>
2017-11-12 20:11:57 -0500

    make it better

--- index.js
+++ index.js
@@ -18,47 +18,97 @@ app.listen(app.get('port'), function() {
 });
 
 
-var request = require('request');
+var request = require('request'),
+    cheerio = require('cheerio');
+var jsdom = require('jsdom');
+const queryString = require('query-string');
 
-var ArticleParser = require('article-parser');
+function getDomain(url) {
+    var hostname;
+    //find & remove protocol (http, ftp, etc.) and get hostname
 
-ArticleParser.configure({
-    timeout: 15 * 1000
-});
-(function go() {
-    request('http://9i9icenter.com/db/getUrl', function(error, response, body) {
-        if (error) {
-            setTimeout(function() {
-                go();
-            }, 15000);
-            console.log('error', error);
-        };
-        if (body && JSON.parse(body) && JSON.parse(body).url) {
-            ArticleParser.extract(JSON.parse(body).url).then(function(article) {
-                console.log('article.content.length', article.content.length);
-                if (article.content.length > 2000) {
-                    request.post({
-                        url: 'http://9i9icenter.com/db/addArticle',
-                        form: article
-                    }, function(error, response, body) {
-                        if (error) {
-                            console.log('error', error);
-                        }
-                        go();
-                    });
-                } else {
-                    go();
-                }
-            }).catch(function(error) {
-                console.log('ArticleParser catch error', error);
-                go();
-            }).finally(function() {
+    if (url.indexOf("://") > -1) {
+        hostname = url.split('/')[2];
+    } else {
+        hostname = url.split('/')[0];
+    }
 
-            });
-        } else {
-            setTimeout(function() {
-                go();
-            }, 15000);
+    //find & remove port number
+    hostname = hostname.split(':')[0];
+    //find & remove "?"
+    hostname = hostname.split('?')[0];
+
+    return hostname;
+}
+var baseUrl = 'http://myui.net.cn:3000/';
+function parse(url) {
+    request(url, function(error, response, body) {
+        if(body === undefined){
+            parse(baseUrl);
+            return;
         }
+        try{
+            var obj = JSON.parse(body);
+        }catch(e){
+            console.log('body error: ', e);
+            parse(baseUrl);
+            return
+        }
+        
+        console.log('obj.url', obj.url);
+        request(obj.url, function(error, response, body) {
+            try {
+                // statements
+                 var $ = cheerio.load(body);
+            } catch(e) {
+                // statements
+                console.log(e);
+                parse(baseUrl);
+                return;
+
+            }
+           
+            var requestBody = [];
+            $('a').each(function(index, a) {
+
+                var text = $(a).text().trim().replace(" ", "");
+                if (!/.*[\u4e00-\u9fa5]+.*$/.test(text)) {
+                    //alert("没有包含中文");
+                    //console.log('no chinese', text);
+                } else {
+                    if ($(this).prop('href')) {
+                        var toQueueUrl = $(this).prop('href').split('#')[0];
+                        //alert("包含中文");
+                        if (toQueueUrl.indexOf("http://") == 0) {
+                            //var objectId = new ObjectID().toString();
+                            var obj = {
+                                url: toQueueUrl,
+                                title: text,
+                                titleLength: text.length,
+                                urlDomain: getDomain(toQueueUrl),
+                                isQueue: false,
+                                isArticle: false,
+                                qualityPercentage: -1
+                            };
+                            requestBody.push(obj);
+                        }
+                    }
+                }
+            });
+
+            request.post({
+                url: baseUrl + 'save',
+                form: {data: JSON.stringify(requestBody)}
+            }, function(error, response, body) {
+                parse(baseUrl);
+                if (error) {
+                    return console.log('error', error)
+                } else {
+                    console.log('body', body);
+                }
+            })
+        })
     })
-})();
+}
+
+parse(baseUrl);

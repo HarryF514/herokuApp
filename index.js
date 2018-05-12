a7d7b31b96602ceb7e7001af8ec6e4f156ac8a66
harryfeng <2307555175@qq.com>
2018-05-11 20:16:51 -0400

    make it better

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-
+require("./queryUrl.js");
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -16,99 +16,3 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
 });
-
-
-var request = require('request'),
-    cheerio = require('cheerio');
-var jsdom = require('jsdom');
-const queryString = require('query-string');
-
-function getDomain(url) {
-    var hostname;
-    //find & remove protocol (http, ftp, etc.) and get hostname
-
-    if (url.indexOf("://") > -1) {
-        hostname = url.split('/')[2];
-    } else {
-        hostname = url.split('/')[0];
-    }
-
-    //find & remove port number
-    hostname = hostname.split(':')[0];
-    //find & remove "?"
-    hostname = hostname.split('?')[0];
-
-    return hostname;
-}
-var baseUrl = 'http://myui.net.cn:3000/';
-function parse(url) {
-    request(url, function(error, response, body) {
-        if(body === undefined){
-            parse(baseUrl);
-            return;
-        }
-        try{
-            var obj = JSON.parse(body);
-        }catch(e){
-            console.log('body error: ', e);
-            parse(baseUrl);
-            return
-        }
-        
-        console.log('obj.url', obj.url);
-        request(obj.url, function(error, response, body) {
-            try {
-                // statements
-                 var $ = cheerio.load(body);
-            } catch(e) {
-                // statements
-                console.log(e);
-                parse(baseUrl);
-                return;
-
-            }
-           
-            var requestBody = [];
-            $('a').each(function(index, a) {
-
-                var text = $(a).text().trim().replace(" ", "");
-                if (!/.*[\u4e00-\u9fa5]+.*$/.test(text)) {
-                    //alert("没有包含中文");
-                    //console.log('no chinese', text);
-                } else {
-                    if ($(this).prop('href')) {
-                        var toQueueUrl = $(this).prop('href').split('#')[0];
-                        //alert("包含中文");
-                        if (toQueueUrl.indexOf("http://") == 0) {
-                            //var objectId = new ObjectID().toString();
-                            var obj = {
-                                url: toQueueUrl,
-                                title: text,
-                                titleLength: text.length,
-                                urlDomain: getDomain(toQueueUrl),
-                                isQueue: false,
-                                isArticle: false,
-                                qualityPercentage: -1
-                            };
-                            requestBody.push(obj);
-                        }
-                    }
-                }
-            });
-
-            request.post({
-                url: baseUrl + 'save',
-                form: {data: JSON.stringify(requestBody)}
-            }, function(error, response, body) {
-                parse(baseUrl);
-                if (error) {
-                    return console.log('error', error)
-                } else {
-                    console.log('body', body);
-                }
-            })
-        })
-    })
-}
-
-parse(baseUrl);
---
+++ queryUrl.js
@@ -0,0 +1,123 @@
+/*
+ * @Author: Harry Feng
+ * @Date:   2017-11-09 16:54:14
+ * @Last Modified by:   Harry Feng
+ * @Last Modified time: 2017-11-10 17:36:28
+ */
+
+var request = require('request'),
+    cheerio = require('cheerio');
+var jsdom = require('jsdom');
+const queryString = require('query-string');
+var exec = require('child_process').exec;
+var myuinetcn = 'http://huoreport.com:2052/';
+var myuinetcnSaveUrl = myuinetcn + 'save';
+var requestTimeOut = 5000;
+
+function getDomain(url) {
+    var hostname;
+    //find & remove protocol (http, ftp, etc.) and get hostname
+
+    if (url.indexOf("://") > -1) {
+        hostname = url.split('/')[2];
+    } else {
+        hostname = url.split('/')[0];
+    }
+
+    //find & remove port number
+    hostname = hostname.split(':')[0];
+    //find & remove "?"
+    hostname = hostname.split('?')[0];
+
+    return hostname;
+}
+
+function parse(url) {
+    request(url, {
+        timeout: requestTimeOut
+    }, function(error, response, body) {
+        if (body === undefined) {
+            parse(myuinetcn);
+            return;
+        }
+        try {
+            var obj = JSON.parse(body);
+        } catch (e) {
+            console.log('body error: ', e);
+            parse(myuinetcn);
+            return
+        }
+
+        console.log('obj.url', obj.url);
+        request(obj.url, {
+            timeout: requestTimeOut
+        }, function(error, response, body) {
+            try {
+                // statements
+                var $ = cheerio.load(body);
+            } catch (e) {
+                // statements
+                console.log(e);
+                parse(myuinetcn);
+                return;
+
+            }
+
+            var requestBody = [];
+            $('a').each(function(index, a) {
+
+                var text = $(a).text().trim().replace(" ", "");
+                if (!/.*[\u4e00-\u9fa5]+.*$/.test(text)) {
+                    //alert("没有包含中文");
+                    //console.log('no chinese', text);
+                } else {
+                    if ($(this).prop('href')) {
+                        var toQueueUrl = $(this).prop('href').split('#')[0];
+                        //alert("包含中文");
+                        if (toQueueUrl.indexOf("http://") == 0) {
+                            //var objectId = new ObjectID().toString();
+                            var obj = {
+                                url: toQueueUrl,
+                                title: text,
+                                titleLength: text.length,
+                                urlDomain: getDomain(toQueueUrl),
+                                isQueue: false,
+                                isArticle: false,
+                                qualityPercentage: -1
+                            };
+                            requestBody.push(obj);
+                        }
+                    }
+                }
+            });
+
+            request.post({
+                url: myuinetcnSaveUrl,
+                form: {
+                    data: JSON.stringify(requestBody)
+                }
+            }, function(error, response, body) {
+                parse(myuinetcn);
+                if (error) {
+                    return console.log('error', error)
+                } else {
+                    console.log('body', body);
+                }
+            })
+        })
+    })
+}
+
+parse(myuinetcn);
+
+setTimeout(function() {
+    exec("forever restart queryUrl.js", function(error, stdout, stderr) {
+        if (error) {
+            console.log(error);
+            return;
+        }
+        if (stdout) {
+            console.log(stdout);
+        }
+    });
+}, 60000);
